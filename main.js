0b9eef678727053705c079e351e64a47f0902d9a
khatunakhomasuridze08 <99075136+khatunakhomasuridze08@users.noreply.github.com>
2022-04-07 13:37:43 +0400

    add arrays,object simple

---
+++ main.js
@@ -0,0 +1,48 @@
+let arr = [9,10,13,11,27];
+let ageSum = arr[0] + arr[1] + arr[2] + arr[3] +arr[4];
+let result = ageSum/arr.length;
+console.log(result);
+
+let myArr = [
+    {
+        name: "nino",
+        age: 25
+    },
+    {
+        name: "damiane",
+        age: 28
+    },
+    {
+        name: "anna",
+        age: 22
+    },
+    {
+        name: "saba",
+        age: 30
+    },
+    {
+        name: "gabriel",
+        age: 27
+    }
+ ]
+// console.log(myArr[1]);
+
+let client = {
+    firstName: "khatuna",
+    lastName: "khomasuridze",
+    address: ["meskhishvili street", "akhalcixis street"],
+    age: 26,
+    phoneNumbers: [5141414141, 57923232323, 2519191]
+}
+// console.log(client,client.address, client.phoneNumbers);
+
+
+let myName = `My name is  ${client.firstName}`;
+console.log(myName);
+let myAge = `My age is ${client.age} year.`;
+console.log(myAge);
+let myAddr = `My address is ${client.address[0]}`;
+console.log(myAddr);
+
+
+
